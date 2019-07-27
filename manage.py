ba25661d40cdbc400b5928419aca4f0695b90619
Paul Ayo <femi4dami@gmail.com>
2019-07-27 21:38:30 +0100

    started adding the bootstrap styles

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estate.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estate_proj.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
