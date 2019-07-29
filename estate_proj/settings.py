4c27d00937594af134920f65201d40b82411e2ff
Paul Ayo <femi4dami@gmail.com>
2019-07-29 03:19:06 +0100

    Included the listing page

--- estate_proj/settings.py
+++ estate_proj/settings.py
@@ -19,6 +19,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
