3e1259cae2024fdfe70f1245b239d4537176fdaf
Paul Oloyede <ayopaulofficial@gmail.com>
2020-02-20 12:53:01 +0100

    clean up some code

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from listings.models import Listing
 
 def index(request):
+    
     return render(request, './pages/index.html')
 
 
