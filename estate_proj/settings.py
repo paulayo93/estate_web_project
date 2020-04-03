8b794481c02ca610010b1f31729688bb190fe317
Paul Oloyede <ayopaulofficial@gmail.com>
2020-04-03 14:13:36 +0100

    remove email settings

--- estate_proj/settings.py
+++ estate_proj/settings.py
@@ -129,14 +129,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'xxxxxx'
-EMAIL_HOST_PASSWORD = 'xxxxx'
-EMAIL_USE_TLS = True
-
 
 try: 
     from .local_settings import *
